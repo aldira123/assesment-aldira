437bacd0c9dc8cd65b9a25706d64563972684756
aldira123 <aldirafithanifah@gmail.com>
2024-11-18 12:51:32 +0700

    fix: delete unused css, font and fix  layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Album from "@/sections/Album";
 
 export default function Home() {
   return (
-    <main id="main">
-      <Hero />
-      <Album />
+    <main className="main">
+        <Hero />
+        <Album />
     </main>
   );
 }
